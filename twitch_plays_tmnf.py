c6e102fc62fa2f97429b31bb351618fdb41a47bc
Benjamin Janssens <benji.janssens@gmail.com>
2020-01-24 16:26:43 +0100

    Added threading of actions to support anarchy

--- twitch_plays_tmnf.py
+++ twitch_plays_tmnf.py
@@ -1,7 +1,6 @@
 import sys
 import getopt
-import time
-import threading
+from threading import Thread
 from twitch_irc import TwitchIRC
 import py_direct_input
 import di_keyboard_scan_codes
@@ -9,17 +8,17 @@ import di_keyboard_scan_codes
 
 def handle(message):
     if "up" == message.lower():
-        py_direct_input.PressAndReleaseKey(
-            di_keyboard_scan_codes.DIK_UP, 1)
+        Thread(target=py_direct_input.PressAndReleaseKey,
+               args=(di_keyboard_scan_codes.DIK_UP, 1)).start()
     if "down" == message.lower():
-        py_direct_input.PressAndReleaseKey(
-            di_keyboard_scan_codes.DIK_DOWN, 1)
+        Thread(target=py_direct_input.PressAndReleaseKey,
+               args=(di_keyboard_scan_codes.DIK_DOWN, 1)).start()
     if "left" == message.lower():
-        py_direct_input.PressAndReleaseKey(
-            di_keyboard_scan_codes.DIK_LEFT, 0.5)
+        Thread(target=py_direct_input.PressAndReleaseKey,
+               args=(di_keyboard_scan_codes.DIK_LEFT, 0.5)).start()
     if "right" == message.lower():
-        py_direct_input.PressAndReleaseKey(
-            di_keyboard_scan_codes.DIK_RIGHT, 0.5)
+        Thread(target=py_direct_input.PressAndReleaseKey,
+               args=(di_keyboard_scan_codes.DIK_RIGHT, 0.5)).start()
 
 
 def main(argv):
